116027027030d78e79eed97f793ab8e268fd8d81
Abhishek <55457501+Abhishek-Dobliyal@users.noreply.github.com>
2021-02-24 22:29:54 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,320 @@
+# Required Imports
+import streamlit as st # pip install streamlit
+from file_processing import FileProcessor
+import tempfile
+import os
+import shutil
+import time
+
+# Helper Functions
+def get_file_data(file):
+    ''' Writes the uploaded file data
+    to a temporary file for processing'''
+    if not os.path.isdir("./temp_files"):
+        os.mkdir("./temp_files")
+
+    tempfile.tempdir = "temp_files" # Set the directory where all temp files will be stores
+    temp_file = tempfile.NamedTemporaryFile(suffix="file_for_processing")
+    temp_file.write(file.read())
+
+    return temp_file
+
+def display_progress_bar():
+    ''' Displays a progress bar to visualize
+    the completion of a function '''
+    processing_txt = st.markdown("#### Processing, Please wait...")
+    bar = st.progress(0)
+    for percent in range(100):
+        time.sleep(0.05)
+        bar.progress(percent)
+
+    bar.empty()
+    processing_txt.empty()
+
+def display_msg(msg, msg_type=0):
+    ''' Display message according to the
+    type 
+    1 -> Success Message
+    0 -> Info Message
+    -1 -> Error Message
+    '''
+    msg_widget = st.empty()
+    
+    if msg_type == 1:
+        msg_widget = st.success(msg)
+        time.sleep(3)
+        msg_widget.empty()
+    
+    elif msg_type == -1:
+        msg_widget = st.error(msg)
+        time.sleep(3)
+        msg_widget.empty()
+    
+    elif msg_type == 0:
+        msg_widget = st.info(msg)
+        time.sleep(3)
+        msg_widget.empty()
+
+def get_file_details(processed_file_path, processed_file_details, file_extension):
+    ''' Dispalys the basic details of the file
+    inside an expander widget '''
+    info_expander = st.beta_expander("Output Section")
+    with info_expander:
+        processed_col, file = st.beta_columns(2)
+        processed_col.write(processed_file_details)
+
+        if file_extension in ("mov", "mp4", "avi"):
+            file.video(processed_file_path)
+        else:
+            file.image(processed_file_path)
+
+def display_processed_file(processed_file_path, processed_files):
+    ''' Displays the processed file in web app '''
+    if not processed_files:
+        display_msg("No files processed yet.", -1)
+
+    elif len(processed_files) == 1:
+        file = processed_files[0]
+        processed_filename, processed_file_ext = os.path.basename(processed_file_path + file).split('.')
+        processed_file_details = {
+            "File Name": processed_filename,
+            "File Type": processed_file_ext,
+            "File Size": str(round(os.path.getsize(processed_file_path + file) / 1e6, 2)) + " MB"
+        }
+
+        if processed_file_ext in ("mov", "mp4", "avi"):
+            get_file_details(processed_file_path + file, processed_file_details, 
+                            processed_file_ext)
+        else:
+            get_file_details(processed_file_path + file, processed_file_details, 
+                            processed_file_ext)
+    
+    else:
+        display_msg("You can only process and download a single file at a time", 0)
+    
+    shutil.rmtree(processed_file_path)
+
+def display_info_sections():
+    ''' Displays the instructions and about section '''
+    help_expander = st.beta_expander("Instructions")
+    about_expander = st.beta_expander("About")
+    instructions = '''
+            * **Choose or Drag N Drop** a file to upload. For a video file, the duration must not exceed 
+            the 30 seconds mark.
+
+            * Once uploaded, select the enhacements you would like to apply from the sidebar.
+
+            * Click on the **Generate** button and wait for the processing to complete.
+
+            * Once completed, click on the **Proceed to Download** button to generate the output.
+
+            * Wait for the processing to complete **(notice the RUNNING status on the top right hand corner)**.
+            Once completed, expand the **Output Section** section to view the output.
+
+            * If satisfied with the results:
+                - If the output is an image file then right click on the file and 
+                select the **Save as** option to download the output.
+                
+                - If it is a video file then click on the **Three dots** present at 
+                the bottom right corner inside the video player and select **Download**.
+
+    '''
+
+    about = '''
+            Made By  <span style="color:seagreen">**Abhishek Dobliyal**</span> :smile: 
+            [![GitHub Abhishek-Dobliyal](https://img.shields.io/github/followers/Abhishek-Dobliyal?label=follow&style=social)](https://github.com/Abhishek-Dobliyal)
+            [![Linkedin: Abhishek Dobliyal]
+            (https://img.shields.io/badge/-AbhishekDobliyal-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/abhishek-dobliyal-4474061b7/)]
+            (https://www.linkedin.com/in/abhishek-dobliyal-4474061b7) 
+
+            #### Modules Utilized:
+            - <a style="text-decoration: none;" href="http://streamlit.io/" target="blank_"> Streamlit </a>
+            - <a style="text-decoration: none;" href="https://opencv.org/releases/" target="blank_"> OpenCV </a>
+            - <a style="text-decoration: none;" href="https://zulko.github.io/moviepy/" target="blank_"> MoviePy </a>
+            - <a style="text-decoration: none;" href="https://pillow.readthedocs.io/en/3.0.x/index.html" target="blank_"> Pillow </a>
+            - <a style="text-decoration: none;" href="https://numpy.org/" target="blank_"> NumPy </a>
+
+            #### Hope You Like It :blush:
+    '''
+    with help_expander:
+        help_expander.markdown(instructions, unsafe_allow_html=True)
+    with about_expander:
+        about_expander.markdown(about, unsafe_allow_html=True)
+
+# Main App
+def main():
+    ''' The main Web app '''
+    # Set Page name and favicon
+    ICON = "./assets/favicon.png"
+    st.set_page_config(page_title="MonoShot", page_icon=ICON)
+
+    # Title
+    TITLE_STYLE = '''
+                border: 3px solid #9ab7d6;
+                border-radius: 15px;
+                text-align: center;
+                background: rgb(220, 235, 237);
+                color: white;
+                font-family: Futura, sans-serif;
+                color: pink;
+                text-shadow: #000 0px 0px 1px;
+            '''
+    title = f'''
+            <div style="{TITLE_STYLE}">
+                <h1> MONOSHOT </h1>
+            </div>
+            '''
+    st.markdown(title, unsafe_allow_html=True) # Render the HTML/CSS
+
+    # Header
+    HEADER = "Just a single shot that's all it takes."
+    st.subheader(HEADER)
+
+    # File Upload and Selections
+    PROCESSED_DATA_PATH = "./processed_data/"
+
+    uploaded_file = st.file_uploader("Upload your file here:",
+                                    type=[".mp4", ".avi", ".mov", ".jpeg", ".jpg", ".png"])
+    if uploaded_file:
+        file_data = get_file_data(uploaded_file)
+        processor = FileProcessor(file_data) # Processor object for applying diff. methods to the media file
+
+        if uploaded_file.type in ("video/mp4", "video/mov", "video/avi"):
+            if not os.path.isdir(PROCESSED_DATA_PATH):
+                os.mkdir(PROCESSED_DATA_PATH)
+
+            video_duration = processor.get_duration()
+            has_required_dim = processor.get_dimensions()
+
+            if video_duration > 30:
+                display_msg("Oops! Video too long to be processed!", -1)
+            
+            elif not has_required_dim:
+                display_msg("Oops! Allowed Resolutions are: 480p, 720p and 1080p", -1)
+
+            else:
+                # SideBar Widgets 
+                select_options = ["Enhance Image", "Generate Shot"]
+                select_output = st.sidebar.selectbox("Select Enhancement:", select_options)
+
+                if select_output:
+                    time.sleep(1.5)
+                    if select_output == "Enhance Image":
+                        time_stamp = st.sidebar.slider("Choose the time stamp (in seconds)", 
+                                                    min_value=1, max_value=video_duration, 
+                                                    step=1)
+                        brightness_lvl = st.sidebar.slider("Brightness:", 
+                                                    min_value=0.0, max_value=2.0, 
+                                                    step=0.2, value=1.0)
+                        sharpness_lvl = st.sidebar.slider("Sharpness:", 
+                                                    min_value=0.0, max_value=2.0, 
+                                                    step=0.2, value=1.0)
+                        contrast_lvl = st.sidebar.slider("Contrast:", 
+                                                    min_value=0.0, max_value=2.0, 
+                                                    step=0.2, value=1.0)
+                        color_lvl = st.sidebar.slider("Color:", 
+                                                    min_value=0.0, max_value=2.0, 
+                                                    step=0.2, value=1.0)
+
+                        if st.sidebar.button("Generate"):
+                            processor.enhanced_img(PROCESSED_DATA_PATH, time_stamp*1000, # Timestamp should be in milliseconds
+                                                    brightness_lvl, sharpness_lvl, 
+                                                    contrast_lvl, color_lvl) 
+                            display_progress_bar()
+                            display_msg("Enhanced Image has been successfully generated.", 1)
+
+                    elif select_output == "Generate Shot":
+                        display_msg("NOTE: It may take a while to generate a shot.", 0)
+
+                        shot_options = ["SlowMo", "TimeLapse", "GIF", "Boomerang"]
+                        shot = st.sidebar.selectbox("Select Shot:", shot_options)
+
+                        if shot == "SlowMo" and st.sidebar.button("Generate"):
+                            processor.generate_shot(PROCESSED_DATA_PATH, slowmo=True)
+                            display_progress_bar()
+                            display_msg("SlowMo has been successfully generated.", 1)
+                        
+                        elif shot == "TimeLapse" and st.sidebar.button("Generate"):
+                            processor.generate_shot(PROCESSED_DATA_PATH, timelapse=True)
+                            display_progress_bar()
+                            display_msg("TimeLapse has been successfully generated.", 1)
+                        
+                        elif shot == "GIF" and st.sidebar.button("Generate"):
+                            processor.generate_shot(PROCESSED_DATA_PATH, gif=True)
+                            display_progress_bar()
+                            display_msg("GIF has been successfully generated.", 1)
+                        
+                        elif shot == "Boomerang":
+                            start_time = st.sidebar.slider("Choose the start time (in seconds):", 
+                                                    min_value=1, max_value=video_duration, 
+                                                    step=1)
+                            end_time = st.sidebar.slider("Choose the end time (in seconds):", 
+                                                    min_value=start_time + 2, max_value=video_duration, 
+                                                    step=1)
+
+                            if st.sidebar.button("Generate"):
+                                processor.generate_shot(PROCESSED_DATA_PATH, 
+                                                       boomerang=(True, start_time, end_time))
+                                display_progress_bar()
+                                display_msg("Boomerang has been successfully generated.", 1)
+
+                else:
+                    display_msg("Please select atleast a single option to proceed.", 0)
+
+        else:
+            if not os.path.isdir(PROCESSED_DATA_PATH):
+                os.mkdir(PROCESSED_DATA_PATH)
+
+            select_options = ["Enhance Resolution", "Apply Filter", "Extract Text"]
+            select_output = st.sidebar.selectbox("Select Enhancement:", select_options)
+
+            if select_output:
+                time.sleep(1.5)
+                if select_output == "Enhance Resolution":
+
+                    if st.sidebar.button("Generate"):
+                        display_msg("Please wait. It may take a while to enhance the resolution...", 0)
+                        processor.enhance_resolution(PROCESSED_DATA_PATH)
+                        display_progress_bar()
+                        display_msg("Enhanced Resolution Image has been successfully generated.", 1)
+                
+                elif select_output == "Apply Filter":
+                    select_options = ["Pencil Sketch", "Water Colored", "Faded", "Document",
+                                     "Cartoonify", "Vigenette", "Phantom", "Negative"]
+                    select_output = st.sidebar.selectbox("Select a filter:", select_options)
+
+                    if st.sidebar.button("Generate"):
+                        processor.apply_filter(PROCESSED_DATA_PATH, filter=select_output)
+                        display_progress_bar()
+                        display_msg("Filter has been successfully applied.", 1)
+
+                elif select_output == "Extract Text":
+                    if st.sidebar.button("Fetch Text"):
+                        txt = processor.extract_txt()
+                    
+                        if txt:
+                            display_msg("Text extraction successful.", 1)
+                            text_expander = st.beta_expander("Output Section")
+                            with text_expander:
+                                st.text("*" * 70)
+                                st.write(txt)
+                                st.text("*" * 70)
+                        else:
+                            display_msg("Failed to extract text!", -1)
+
+            else:
+                display_msg("Please select atleast a single option to proceed.", 0)
+
+        file_data.close() # Delete the temp file after all the operations. 
+
+        helper_widget = st.empty()
+        processed_files = os.listdir(PROCESSED_DATA_PATH)
+
+        if processed_files:
+            if helper_widget.button("Proceed to Download"):
+                    display_processed_file(PROCESSED_DATA_PATH, processed_files)
+
+    display_info_sections()
+
+if __name__ == '__main__':
+    main()
---
+++ file_processing.py
@@ -0,0 +1,256 @@
+# Required Imports
+import cv2 # pip install opencv-python
+from cv2 import dnn_superres # pip install opencv-contrib-python
+from moviepy.editor import * # pip install moviepy
+import moviepy.video.fx.all as vfx
+from PIL import Image, ImageEnhance # pip install pillow
+import numpy as np # pip install numpy
+import pytesseract as pyt # pip install pytesseract
+
+# VideoProcessor class for all processing related methods
+class FileProcessor:
+    def __init__(self, file):
+        self.__file = file.name # Fetch the location of the file for processing
+    
+    def get_duration(self):
+        ''' Get the duration of video file in 
+            seconds '''
+        video = cv2.VideoCapture(self.__file)
+        frames = video.get(cv2.CAP_PROP_FRAME_COUNT) 
+        fps = int(video.get(cv2.CAP_PROP_FPS)) 
+        duration = int(frames / fps)
+
+        return duration
+
+    def get_dimensions(self):
+        ''' Get the dimesnions of file '''
+        file = cv2.VideoCapture(self.__file)
+        width, height = file.get(3), file.get(4)
+
+        # Return true if the file uploaded has dimensions btw 480p and 1080p
+        return width in range(852, 1921) and height in range(480, 1081) 
+        
+    def generate_shot(self, output_path, slowmo=False, 
+                       timelapse=False, gif=False, 
+                       boomerang=(False, None, None)):
+        ''' Generates specified shot of the 
+        video file '''
+        if slowmo or timelapse:
+            video = cv2.VideoCapture(self.__file)
+            FW, FH = int(video.get(3)), int(video.get(4))
+            FOUR_CC = cv2.VideoWriter_fourcc(*"AVC1")
+
+            if slowmo:
+                FPS = 5.5
+                output = cv2.VideoWriter(f"./{output_path}/slow_motion.mp4", FOUR_CC, FPS, (FW, FH))
+                while video.isOpened():
+                    ret, frame = video.read()
+                    if not ret:
+                        break
+                    output.write(frame)
+
+            elif timelapse:
+                FPS = 30
+                count, speed = 0, 10
+                frames = []
+                output = cv2.VideoWriter(f"./{output_path}/timelapse.mp4", FOUR_CC, FPS, (FW, FH))
+
+                while video.isOpened():
+                    ret, frame = video.read()
+                    if not ret:
+                        break
+
+                    if count % speed == 0:
+                        frames.append(frame)
+
+                    count += 1
+
+                for frame in frames:
+                    output.write(frame)
+        
+            video.release()
+            output.release()
+
+        elif gif:
+            video_file = VideoFileClip(self.__file).resize(0.6)
+            video_file.write_gif(f"./{output_path}/sample.gif")
+        
+        elif boomerang[0]:
+            start, end = boomerang[1], boomerang[2]
+            video_file = VideoFileClip(self.__file).resize(0.6)
+
+            clip = video_file.subclip(start, end) # Get subclip from the video file
+            clip = clip.fx(vfx.crop, x1=115, x2=399, y1=0, y2=288)
+
+            speed_clip = clip.speedx(2)
+            reversed_speed_clip = speed_clip.fx(vfx.time_mirror)
+
+            final = concatenate_videoclips([speed_clip, reversed_speed_clip]) # Merge the clips
+            final.to_gif(f"./{output_path}/boomerang.gif", fps=25)
+
+    def enhanced_img(self, output_path, timestamp, brightness_lvl,
+                    sharpness_lvl, contrast_level, color_level):
+        ''' Extar image from a specified
+        video time stamp and generate an enhanced 
+        version of it'''
+        TEMP_IMG_PATH = "./temp_files/original_enhanced_img.png"
+        video = cv2.VideoCapture(self.__file)
+        video.set(cv2.CAP_PROP_POS_MSEC, timestamp)
+        video.set(3, 1920)
+        video.set(4, 1080)
+        _, frame = video.read()
+        cv2.imwrite(TEMP_IMG_PATH, frame)
+        video.release()
+
+        # Normalize image to reduce noise
+        img = cv2.imread(TEMP_IMG_PATH, cv2.IMREAD_UNCHANGED)
+        normalized_img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX) 
+
+        # Apply final enhancements according to parameters
+        rgb_img = cv2.cvtColor(normalized_img, cv2.COLOR_BGR2RGB)
+        pil_img = Image.fromarray(rgb_img)
+
+        if brightness_lvl != 1.0:
+            pil_img = ImageEnhance.Brightness(pil_img).enhance(brightness_lvl)
+        elif sharpness_lvl != 1.0:
+            pil_img = ImageEnhance.Sharpness(pil_img).enhance(sharpness_lvl)
+        elif contrast_level != 1.0:
+            pil_img = ImageEnhance.Contrast(pil_img).enhance(contrast_level)
+        elif color_level != 1.0:
+            pil_img = ImageEnhance.Color(pil_img).enhance(color_level)
+
+        pil_img.save(f"{output_path}/enhanced_img.png")
+
+    def enhance_resolution(self, output_path):
+        ''' Enhance the resolution of the image
+        using Super Resolution technique '''
+        MODEL_PATH = "./assets/FSRCNN_x4.pb" 
+
+        img = cv2.imread(self.__file)
+
+        # Apply DNN Super Resolution technique using pre trained model (in our case FSRCNN model)
+        sr = dnn_superres.DnnSuperResImpl_create()
+        sr.readModel(MODEL_PATH)
+        sr.setModel("fsrcnn", 4)
+
+        final_img = sr.upsample(img)
+
+        final_img = cv2.fastNlMeansDenoisingColored(final_img, None, 10, 10, 7, 15) # Remove Noise
+        cv2.imwrite(f"./{output_path}/enhanced_resolution.png", final_img)
+
+    def apply_filter(self, output_path, filter=None):
+        ''' Apply specified filter to the image '''
+        img = cv2.imread(self.__file, cv2.IMREAD_UNCHANGED)
+
+        if filter == "Pencil Sketch":
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            inverted_img = cv2.bitwise_not(gray)
+            smooth_img = cv2.GaussianBlur(inverted_img, (21,21), sigmaX=0, sigmaY=0)
+
+            final_img = cv2.divide(gray, 255 - smooth_img, scale=256)
+            cv2.imwrite(f"./{output_path}/pencil_sketch.png", final_img)
+        
+        elif filter == "Faded":
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"./{output_path}/faded.png", gray)
+        
+        elif filter == "Water Colored":
+            final_img = cv2.stylization(img, sigma_s=60, sigma_r=0.07)
+            cv2.imwrite(f"./{output_path}/water_colored.png", final_img)
+        
+        elif filter == "Cartoonify":
+            smooth_img = cv2.bilateralFilter(img, 10, 250, 250)
+
+            # Work on edge lines
+            gray = cv2.cvtColor(smooth_img, cv2.COLOR_BGR2GRAY)
+            img_blur = cv2.medianBlur(gray, 5)
+            img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                            cv2.THRESH_BINARY, 7, 2)
+
+            # Multiply the original and edge lined img
+            final_img = cv2.bitwise_and(smooth_img, smooth_img, mask=img_edge)
+            cv2.imwrite(f"./{output_path}/cartoonified.png", final_img)
+        
+        elif filter == "Document":
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            adaptive_thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                    cv2.THRESH_BINARY, 151, 10)
+            final_img = cv2.medianBlur(adaptive_thresh, 3)
+            
+            cv2.imwrite(f"./{output_path}/document.png", final_img)
+        
+        elif filter == "Vigenette":
+            rows, cols = img.shape[:2]
+
+            # Generating vignette mask using Gaussian kernels
+            kernel_x = cv2.getGaussianKernel(cols,450)
+            kernel_y = cv2.getGaussianKernel(rows,450)
+            kernel = kernel_y * kernel_x.T
+            kernel = kernel / kernel.max()
+
+            final_img = np.copy(img)
+            final_img[:, :, :] = 0
+            final_img[:, :, 0] = img[:, :, 0] * kernel
+            final_img[:, :, 1] = img[:, :, 1] * kernel
+            final_img[:, :, 2] = img[:, :, 2] * kernel
+
+            cv2.imwrite(f"./{output_path}/vigenette.png", final_img)
+        
+        elif filter == "Phantom":
+            # Kernel to apply the effect
+            kernel = np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
+            final_img = cv2.filter2D(img, -1, kernel) # Apply a Filter with given kernel
+
+            cv2.imwrite(f"./{output_path}/phantom.png", final_img)
+        
+        elif filter == "Negative":
+            final_img = cv2.bitwise_not(img)
+            cv2.imwrite(f"./{output_path}/negative.png", final_img)
+
+    def extract_txt(self):
+        ''' Extract text from images '''
+        img = cv2.imread(self.__file)
+
+        # Convert image into Grayscale and then apply threshold
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+
+        # Kernel to detect sentences (around rectangle)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+
+        # Dilation to create enlarged image of same shape
+        dilated = cv2.dilate(threshold, kernel, 1)
+        
+        # Find contours
+        contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL,  
+                                      cv2.CHAIN_APPROX_NONE) 
+
+        text = ""
+        for cnt in contours:
+            x, y, w, h = cv2.boundingRect(cnt) # Get the coordinated where txt is located 
+
+            cropped_txt_img = img[y:y+h, x:x+w] 
+
+            text += pyt.image_to_string(cropped_txt_img)
+        
+        # " \n\x0c" is a string that is returned when the img does not contain any text 
+        if text !=  " \n\x0c":
+            return text
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
